57c8a4887bc8db4ebe33e8901a1f8d8d7ebe2c1e
st0n3d-rs <142694185+st0n3d-rs@users.noreply.github.com>
2023-08-26 23:40:40 +0200

    Delete exports.sh

--- strs-iobroker/exports.sh
+++
@@ -1,5 +0,0 @@
-# iobroker runs in host networking mode, so does not use the app proxy which is needed to generate the Tor HS hostname
-# This generates a fake HS hostname for iobroker
-IOBROKER_TOR_DIR="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}"
-mkdir -p "${IOBROKER_TOR_DIR}"
-touch "${IOBROKER_TOR_DIR}/hostname"
